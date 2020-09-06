7301dc346dd2033870122bf1016157f5ec8bad7c
toomec2105 <toomec2105@interia.pl>
2020-09-06 15:20:53 +0200

    add more countryApi tests

--- src/test/java/com/tomek/web_jpa_2/country/CountryDataFetcherFromApiIntegrationTest.java
+++ src/test/java/com/tomek/web_jpa_2/country/CountryDataFetcherFromApiIntegrationTest.java
@@ -24,4 +24,19 @@ public class CountryDataFetcherFromApiIntegrationTest {
 
 		Assertions.assertNotNull(countryJson);
 	}
+	@Disabled
+	@Test
+	void givenUrl_whenFetchFromCountryApi_returnsJsonRandomCountryData() {
+		String countryJson = countryDataFetcher.getCountryData("https://restcountries.eu/rest/v2/all");
+
+		Assertions.assertNotNull(countryJson);
+		
+		Assertions.assertTrue(countryJson.contains("\"name\":\"Greece\""));
+		Assertions.assertTrue(countryJson.contains("\"capital\":\"Athens\""));
+		Assertions.assertTrue(countryJson.contains("\"region\":\"Europe\",\"subregion\":\"Southern Europe\""));
+		Assertions.assertTrue(countryJson.contains("\"nativeName\":\"Österreich\""));
+		Assertions.assertTrue(countryJson.contains("\"capital\":\"Vienna\""));		
+		Assertions.assertTrue(countryJson.contains("\"name\":\"French\",\"nativeName\":\"français\""));
+		Assertions.assertTrue(countryJson.contains("\"name\":\"Latvian\",\"nativeName\":\"latviešu valoda\""));
+	}
 }
--- src/test/java/com/tomek/web_jpa_2/country/CountryDataFetcherFromApiUnitTest.java
+++ src/test/java/com/tomek/web_jpa_2/country/CountryDataFetcherFromApiUnitTest.java
@@ -12,17 +12,29 @@ class CountryDataFetcherFromApiUnitTest {
 		String countryJson = countryDataFetcher.getCountryData("https://restcountries.eu/rest/v2/all");
 
 		Assertions.assertNotNull(countryJson);
+		}
+	@Test
+	void givenUrl_whenFetchFromCountryApi_returnsJsonRandomCountryData() {
+		String countryJson = countryDataFetcher.getCountryData("https://restcountries.eu/rest/v2/all");
+
+		Assertions.assertNotNull(countryJson);
 		
 		Assertions.assertTrue(countryJson.contains("\"name\":\"Greece\""));
 		Assertions.assertTrue(countryJson.contains("\"capital\":\"Athens\""));
 		Assertions.assertTrue(countryJson.contains("\"region\":\"Europe\",\"subregion\":\"Southern Europe\""));
-	
 		Assertions.assertTrue(countryJson.contains("\"nativeName\":\"Österreich\""));
-		Assertions.assertTrue(countryJson.contains("\"capital\":\"Vienna\""));
-		
+		Assertions.assertTrue(countryJson.contains("\"capital\":\"Vienna\""));		
 		Assertions.assertTrue(countryJson.contains("\"name\":\"French\",\"nativeName\":\"français\""));
 		Assertions.assertTrue(countryJson.contains("\"name\":\"Latvian\",\"nativeName\":\"latviešu valoda\""));
+
 	}
 	
+	@Test
+	void givenUrl_fetchesData_withSuitableLength() {
+		String countryJson = countryDataFetcher.getCountryData("https://restcountries.eu/rest/v2/all");
+
+		Assertions.assertNotNull(countryJson);
+		org.assertj.core.api.Assertions.assertThat(countryJson.length()).isGreaterThan(200000);
+		}
 	
 }
